a7c29f374c8c6ab2bee72394eb256460f8653822
Patrick Denzler <patrick.d.denzler@campus.tu-berlin.de>
2018-12-02 16:34:23 +0100

    PP: Add Path to CMakeLists and list of tuples

--- src/auto_smart_factory/include/agent/Path.h
+++ src/auto_smart_factory/include/agent/Path.h
@@ -1,11 +1,17 @@
 #ifndef AGENT_PATH_H_
 #define AGENT_PATH_H_
 
+#include <tuple>
+#include <list>
+
 class Path {
 
 public:
     Path();
     virtual ~Path();
+
+private:
+    std::list<std::tuple<float, float>> points;
 
 };
 
