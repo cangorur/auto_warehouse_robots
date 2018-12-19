ed33625b291adaeb1c98d880d6b5abe1dd52285e
Florian Ziesche <florian.ziesche2@ewetel.net>
2018-12-19 15:02:04 +0100

    TH: Implement first version of Task class for task handler

--- src/auto_smart_factory/include/agent/Task.h
+++ src/auto_smart_factory/include/agent/Task.h
@@ -2,17 +2,51 @@
 #define AGENT_TASK_H_
 
 #include "ros/ros.h"
+#include "agent/path_planning/OrientedPoint.h"
+#include "agent/path_planning/Path.h"
 
 class Task
 {
   public:
-    explicit Task();
+	explicit Task(unsigned int id, uint32_t sourceID, OrientedPoint sourcePos, 
+				uint32_t targetID, OrientedPoint targetPos, Path sourcePath, Path targetPath);
 
-    virtual ~Task();
+	virtual ~Task();
+
+	unsigned int getId();
+
+	uint32_t getSourceTrayId();
+
+	OrientedPoint getSourcePosition();
+
+	uint32_t getTargetTrayId();
+
+	OrientedPoint getTargetPosition();
+
+	Path* getPathToTarget();
+
+	Path* getPathToSource();
 
   private:
-    unsigned int id;
-    
+	// the task id of the task
+	unsigned int id;
+
+	// source Tray id
+	uint32_t sourceId;
+	// source Tray position
+	OrientedPoint sourcePosition;
+
+	// target Tray id
+	uint32_t targetId;
+	// target Tray position
+	OrientedPoint targetPosition;
+
+	// calculated path from source to target (may be Theta* or RRT*)
+	Path pathToTarget;
+
+	// Path to source
+	Path pathToSource;
+
 };
 
 #endif /* AGENT_TASK_H_ */
--- src/auto_smart_factory/src/agent/Task.cpp
+++ src/auto_smart_factory/src/agent/Task.cpp
@@ -1,7 +1,42 @@
 #include "agent/Task.h"
 
-Task::Task()
-{
+Task::Task(unsigned int id, uint32_t sourceID, OrientedPoint sourcePos, 
+			uint32_t targetID, OrientedPoint targetPos, Path sourcePath, Path targetPath) : 
+	id(id), 
+	sourceId(sourceID),
+	sourcePosition(sourcePos),
+	targetId(targetID),
+	targetPosition(targetPos),
+	pathToSource(sourcePath),
+	pathToTarget(targetPath) {
 }
 
 Task::~Task() = default;
+
+unsigned int Task::getId(){
+	return id;
+}
+
+uint32_t Task::getSourceTrayId(){
+	return sourceId;
+}
+
+OrientedPoint Task::getSourcePosition(){
+	return sourcePosition;
+}
+
+uint32_t Task::getTargetTrayId(){
+	return targetId;
+}
+
+OrientedPoint Task::getTargetPosition(){
+	return targetPosition;
+}
+
+Path* Task::getPathToTarget(){
+	return &pathToTarget;
+}
+
+Path* Task::getPathToSource(){
+	return &pathToSource;
+}
