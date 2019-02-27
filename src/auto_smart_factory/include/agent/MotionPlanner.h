91d1684d07a66ee3d26d4251b2092aeb5e52f2ff
Patrick Denzler <patrick.d.denzler@campus.tu-berlin.de>
2019-02-27 13:02:48 +0100

    MP: Add comments to pid and motion planner

--- src/auto_smart_factory/include/agent/MotionPlanner.h
+++ src/auto_smart_factory/include/agent/MotionPlanner.h
@@ -18,17 +18,27 @@
 
 class Agent;
 
-/* The motion planner component manages all movement related stuff ongoing at the agents side.
- * It coverts information about the current position as well as orientation and  a list of points
- * called path to a certain velocity and publishes it to the motion actuator topic to move the robot.
- * This implementation of motion planning only considers a differential drive and
- * is only able to drive straight forward or backwards or turn on spot - so no curve driving except of
- * some path correction while driving straight forward or backward.
- * The motion planner needs to be fed continously with current pose data to work as expected.*/
+/* 
+ * The Motion Planner controls the velocity (linear and angular) of the robot.
+ * It takes a path as a list of points and follows the path given the precision contraints set in the header file.
+ * The Motion Planner takes the poseCallback in the Agent.h to call it's update function whenever the robots position changes.
+ */
 class MotionPlanner {
 
 public:
-	enum class Mode {READY, PID, TURN, FINISHED, STOP, RECOVERY, ALIGN, WAIT, FORWARD, BACKWARD};
+	/*
+	 * Contains the current mode of the motion planner
+	 * READY: The motion planner has a valid path and is ready to drive. This state will automatically turn into PID, TURN or FINISHED in the next iteration
+	 * PID: The pid controller is currently in charge of the robots motion
+	 * TURN: The robot performs an on-spot-turn while following a path and will become READY as soon as the turn is finished
+	 * FINISHED: The robot reached the last waypoint of the path
+	 * STOP: The robot was stopped manually - e.g., by calling stop()
+	 * ALIGN: The robot should align towards a target or into a direction manually - e.g., by calling alignTowards(...)
+	 * WAIT: The robots waits for the departure time of a path point to be reached
+	 * FORWARD: The robot drives forward for a manually given distance - e.g. by calling driveForward(distance)
+	 * BACKWARD: The robot drives backward for a manually given distance e.g. by calling driveBackward(distance)
+	 */ 
+	enum class Mode {READY, PID, TURN, FINISHED, STOP, ALIGN, WAIT, FORWARD, BACKWARD};
 
 	/* Constructor that hands over some the robot configuration as well as
 	 * the motion acutator publisher.
@@ -36,126 +46,202 @@ public:
 	 * @param motion_pub: publisher for the motion actuator topic*/
 	MotionPlanner(Agent* agent, auto_smart_factory::RobotConfiguration robot_config, ros::Publisher* motion_pub);
 
-	virtual ~MotionPlanner();
-
-	/* Hands over the current position as well as the current orientation of the robot on the current
-	 * map and determines whether to drive the current path or not.
+	/* 
+	 * Updates the current position and orientation of the robot given the input (usually called by the poseCallback)
+	 * Performs the driving operation based on the current Mode and adjusts the mode accordinly
 	 * @param position: current position of the agent
 	 * @param orientation: current orientation of the agent*/
 	void update(geometry_msgs::Point position, double orientation);
 
-	/* Align the robots towards a target / direction */
+	/* 
+	 * Align the robots towards a target or direction 
+	 * Variant 1: @param Point target to align towards that target
+	 * Variant 2: @param double direction to align towards a direction 
+	 */
 	void alignTowards(Point target);
 	void alignTowards(double direction);
 
-	/* Drive straight forward or backward for given distance */
+	/* 
+	 * Drive straight forward or backward for given distance 
+	 * @param double distance the distance to drive
+	 */
 	void driveForward(double distance);
 	void driveBackward(double distance);
 
+	/* 
+	 * Give the Motion Planner a new path to drive. 
+	 * The Motion Planner will move into READY state, if the path is valid
+	 */
 	void newPath(Path path);
 
+	/*
+	 * Returns the current mode of the Motion Planner
+	 * @return Mode mode
+	 */
 	Mode getMode();
 
+	/*
+	 * Start: Set the robot state to READY
+	 * Stop: Set the robot state to STOP
+	 */
 	void start();
 	void stop();
 
+	/*
+	 * Returns true if the Motion Planner is in FINISHED state
+	 */ 
 	bool isDone();
+
+	/*
+	 * Returns true if the Motion Planner is in STOP state
+	 */
 	bool isStopped();
+
+	/*
+	 * Returns true if the Motion Planner has a current path
+	 * This will also be true, if the path is already driven
+	 */
 	bool hasPath();
+
+	/*
+	 * Returns true if the current linear velocity is below zero
+	 */
 	bool isDrivingBackwards();
 
+	/*
+	 * Return the current Position of the robot as an OrientedPoint
+	 */
 	OrientedPoint getPositionAsOrientedPoint();
 
+	/*
+	 * Returns true, if the position of the robot has been updated at least once (i.e. the poseCallback called update() at least once)
+	 */
 	bool isPositionInitialized();
 
-private:
-	/* Follow a path using the pid */
+	/*
+	 * virtually destructs the MotionPlanner
+	 */
+	virtual ~MotionPlanner();
+
+  private:
+	/* 
+	 * Follow a path using the pid controller
+	 */
 	void followPath();
 
-	/* Turn the robot on spot facing orientation when finished */
+	/* 
+	 * Variant 1: Turn the robot on spot facing towards the given target when finished
+	 * Variant 2: Turn the robot on spot facing the given direction when finished 
+	 */
 	void turnTowards(Point target);
 	void turnTowards(double direction);
 
-	/* Drive the robot straight forward or backward based on mode for given distance */
+	/* 
+	 * Drive the robot straight forward or backward based on mode 
+	 * for given driveDistance (set with driveForward() or driveBackward()) 
+	 */
 	void driveStraight();
 
-	/* Functions to handle Waypoints */
+	/* 
+	 * Returns true, if the robot is within distToReachPoint / distToReachFinalPoint
+	 */
 	bool isWaypointReached();
+
+	/*
+	 * Returns true, if the current point is the last point in the path
+	 */
 	bool isCurrentPointLastPoint();
 
+	/*
+	 * Set the currentTarget to the next point in the path
+	 */
 	void advanceToNextPathPoint();
 	
-	/* Calculate rotation to target */
+	/* 
+	 * Calculate the rotation angle to the target (in rad) given
+	 * @param Position currentPosition the robots current position
+	 * @param Point targetPosition the target position
+	 */
 	double getRotationToTarget(Position currentPosition, Point targetPosition);
 
-	/* Linear Velocity calculation */
-	double calculateLinearVelocity(double cte);
+	/*
+	 * Calculate the distance to the final waypoint
+	 * Summing the distances between all waypoints
+	 */
 	double getDistanceToTarget();
-	double getNextCurveAngle(double distance);
 
-	/* Helper function to publish the velocity on the robots motion topic */
+	/*
+	 * Pusblish velocity to the motionPub topic and update class attributes for liner and angular velocity
+	 */
 	void publishVelocity(double speed, double angle);
 	
+	/*
+	 * Removes last published path in RVIZ when path is finished
+	 */
 	void publishEmptyVisualisationPath();
 
-	/// information about the current role of the agent
+	// information about the current role of the agent
 	auto_smart_factory::RobotConfiguration robotConfig;
 
-	/// Publisher for the motion actuator topic
+	// Publisher for the motion actuator topic
 	ros::Publisher* motionPub;
 
-	/// Publisher for the visualization messages (rviz)
+	// Publisher for the visualization messages (rviz)
 	ros::Publisher pathPub;
 
-	/// the current mode
+	// the current mode
 	Mode mode = Mode::STOP;
 
-	/// Current Velocities
+	// most recent published linear and angular velocity
 	double currentLinearVelocity = 0.0;
 	double currentAngularVelocity = 0.0;
 	
-	/// the current path of the robot
+	// the current path of the robot
 	Path pathObject;
 	
-	/// the current target for the robot with its index
+	// the current target for the robot with its index
 	Point currentTarget;
 	int currentTargetIndex = -1;
 
+	// the previous target for the robot with its index
 	Point previousTarget;
 	int previousTargetIndex = -1;
 
-	/// Members for turnTowards functionality
+	// members for turnTowards functionality
 	Point alignTarget;
-	double alignDirection = -1;
+	double alignDirection = 0;
+	bool useDirectionForAlignment = false;
 
-	/// Members for driveForward and driveBackward functionality
+	// members for driveForward and driveBackward functionality
 	Position driveStartPosition;
 	double driveDistance = 0.0;
 
-	/// Threshold when robot should stop and turn on spot instead of pid controlled
+	// threshold when robot should stop and turn on spot instead of driving pid controlled
 	double turnThreshold = Math::toRad(55.f);
 	double turnThresholdFirstPoint = Math::toRad(15.f);
 	
-	/// Turning and Driving Speed Limitations from robots config file
+	// turning and driving speed limitations from robots config file
 	float maxTurningSpeed;
 	float minDrivingSpeed;
 	float maxDrivingSpeed;
 
-	/// Precision configuration to reach points
+	// precision configuration to reach points accurately
 	float distToReachPoint = 0.3f;
 	float distToReachFinalPoint = 0.02f;
 	float distToSlowDown = 0.7f;
 	float minPrecisionDrivingSpeed = 0.1f;
 
-	/// Will be true when position is updated the first time
+	// will be true when position is updated the first time
 	bool positionInitialized = false;
 
 protected:
 	Agent* agent;
 	std::string agentID;
 
+	// the pid controller instance controlling the angular velocity
 	PidController* steerPid;
 
+	// the current position of the robot
 	Position pos;
 };
 
