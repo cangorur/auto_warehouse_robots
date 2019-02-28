b3d0b0cc10d434a5eafd835597d6832c91227e63
Vincent Wölfer <vincent.woelfer@gmail.com>
2019-02-28 23:50:59 +0100

    Add more code comments

--- src/auto_smart_factory/include/agent/path_planning/ThetaStarPathPlanner.h
+++ src/auto_smart_factory/include/agent/path_planning/ThetaStarPathPlanner.h
@@ -63,8 +63,7 @@ private:
 	 * @return The smoothed path */
 	Path smoothPath(Path source) const;
 	
-	/**
-	 * Determines if a path corner should be smoothed
+	/** Determines if a path corner should be smoothed
 	 * @param prev Previous path point
 	 * @param curr Current path point
 	 * @param next Next path point
@@ -72,34 +71,81 @@ private:
 	 * @return True iff the corner should be smoothed */
 	bool shouldSmoothCorner(Point prev, Point curr, Point next, double waitTimeAtCenter) const;
 
-	// Return curve which replaces the center input point
-	/** 
-	 * @param prev 
-	 * @param curr 
-	 * @param next 
-	 * @param lastPointInOutput 
-	 * @return */
+	/** Transform three points into curve which replaces the center input point 
+	 * @param prev first point before curce
+	 * @param curr curve center point
+	 * @param next point after curve
+	 * @param lastPointInOutput last point pf previous curve
+	 * @return list of curve points */
 	std::vector<Point> createCurveFromCorner(Point prev, Point curr, Point next, Point lastPointInOutput) const;
+	
+	/** Add more points to a curve
+	 * @param curveStart first curve point
+	 * @param center curve center point
+	 * @param curveEnd last curve point
+	 * @param pointsToAdd number of points to insert
+	 * @return new list of curve points */
 	std::vector<Point> addPointsToCurve(Point curveStart, Point center, Point curveEnd, int pointsToAdd) const;
+	
+	/** Smoothes a list of points using gradient decent
+	 * @param input list of points to smooth 
+	 * @return list of smoothed curve points */
 	std::vector<Point> smoothCurve(const std::vector<Point>& input) const;
 
+	/** Compute curve start/end point (= curve edge)
+	 * @param neighbour neighbouring point not part of the curve
+	 * @param center curve center point
+	 * @return Curve edge point
+	 * */
 	Point getCurveEdge(Point neighbour, Point center) const;
+
+	/** Compute curve start/end point (= curve edge)
+	 * @param neighbour neighbouring point not part of the curve
+	 * @param center curve center point
+	 * @param lastPointInOutput last point of previous curve
+	 * @return Curve edge point
+	 * */
 	Point getCurveEdge(Point neighbour, Point center, Point lastPointInOutput) const;
+	
+	/** Compute angle between two line segments 
+	 * @param prev first point 
+	 * @param curr center point
+	 * @param next last point
+	 * @return angle between the two segments */
 	double getAngle(const Point& prev, const Point& curr, const Point& next) const;
 
-	// Query information
+	// ==== Query information ====
+	// Theta star map used for theta star path queries
 	ThetaStarMap* map;
+	
+	// Robot hardware profile in use
 	RobotHardwareProfile* hardwareProfile;
+	
+	// Used timing calculator
 	TimingCalculator timing;
 	
+	// Path start point
 	OrientedPoint start;
+	
+	// Path target point
 	OrientedPoint target;
+	
+	// Used path start node
 	const GridNode* startNode;
-	const GridNode* targetNode;	
+	
+	// Path target node
+	const GridNode* targetNode;
+	
+	// Path starting time offset
 	double startingTime;
+	
+	// Reservation duration at path target
 	double targetReservationTime;
+	
+	// Is this path query valid?
 	bool isValidPathQuery;
 	
+	// List of reservations to ignore/use smaller variant for
 	std::vector<Rectangle> smallerReservations;
 };
 
--- src/auto_smart_factory/include/agent/path_planning/TimingCalculator.h
+++ src/auto_smart_factory/include/agent/path_planning/TimingCalculator.h
@@ -6,32 +6,87 @@
 #include "OrientedPoint.h"
 #include "Point.h"
 
+// Define special namespace with enum class for more sophisticated parameters. Also don't clutter public namespace
 namespace UncertaintyDirection {
 	enum class Direction {BEHIND, AHEAD};	
 }
-
 using namespace UncertaintyDirection;
 
+// Utility class to perform timing calculations
 class TimingCalculator {
 public:	
 	explicit TimingCalculator() = default;
+	
+	/** Constructor
+	 * @param startingTime The starting time of the path query 
+	 * @param startPoint The starting point of the path query
+	 * @param hardwareProfile The used hardware profile of the robot */
 	TimingCalculator(double startingTime, OrientedPoint startPoint, RobotHardwareProfile* hardwareProfile);
 	
+	/** Calculate time uncertainty for planning
+	 * @param time start time
+	 * @param direction Uncertainty behind or in front of the current time?
+	 * @return calculated timing uncertainty for planning */
 	double getPlanningUncertainty(double time, Direction direction) const;
+
+	/** Calculate time uncertainty for reserving
+	 * @param time start time
+	 * @param direction Uncertainty behind or in front of the current time?
+	 * @return calculated timing uncertainty for reserving */
 	double getReservationUncertainty(double time, Direction direction) const;
+
+	/** Calculate relative time uncertainty
+	 * @param time start time
+	 * @return calculated relative timing uncertainty */
 	double getRelativeUncertainty(double time) const;
 	
+	/** Calculate estimated time for driving and turning to the next path node
+	 * @param current Current path node
+	 * @param target Target path node
+	 * @return estimated driving + turning time */
 	double getDrivingAndTurningTime(ThetaStarGridNodeInformation* current, ThetaStarGridNodeInformation* target) const;
+	
+	/** Estimate turning time until next line segment
+	 * @param prev Previous line segment start
+	 * @param curr Current position
+	 * @param next Next line segment to rotate towards
+	 * @return Estimated turning time */
 	double getTurningTime(Point prev, Point curr, Point next) const;
+
+	/** Estimate turning time until next line segment
+	 * @param startRotationInDeg Current rotation
+	 * @param curr Current position
+	 * @param next Next line segment to rotate towards
+	 * @return Estimated turning time */
 	double getTurningTime(double startRotationInDeg, Point curr, Point next) const;
+	
+	/** Does the robot plan to perform a on spot turn for this corner?
+	 * @param prev Previous line segment start
+	 * @param curr Current position
+	 * @param next Next line segment to rotate towards  
+	 * @param firstPoint Is this the first path point?
+	 * @return True iff a on spot turn will be performed for this corner */
 	bool performsOnSpotTurn(Point prev, Point curr, Point next, bool firstPoint) const;
+
+	/** Does the robot plan to perform a on spot turn for this corner?
+	 * @param startRotationInDeg Current rotation
+	 * @param curr Current position
+	 * @param next Next line segment to rotate towards  
+	 * @param firstPoint Is this the first path point?
+	 * @return True iff a on spot turn will be performed for this corner */
 	bool performsOnSpotTurn(double startRotationInDeg, Point curr, Point next, bool firstPoint) const;
 
 private:
+	// Path starting time
 	double startingTime;
+	
+	// Path starting point with orientation
 	OrientedPoint startPoint;
+	
+	// Robots hardware profile used for estimating timings and battery consumption
 	RobotHardwareProfile* hardwareProfile;
 	
+	// Internal uncertainty computation used by the public ones
 	double getUncertainty(double time) const;
 };
 
